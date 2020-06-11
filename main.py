d12f359e06212cad855affa5ddcb902730075e9d
benjaminellison <scorpio.breeze@gmail.com>
2020-06-11 07:24:27 -0700

    more than one sprite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 import random, time
 
@@ -16,7 +16,6 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
-SPEED = 5
 
 # Set up the screen
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -24,25 +23,26 @@ DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Contagion Simulator")
 
 # Set up sprites
-P1 = person.Person(BLUE)
+#P1 = person.Person(DISPLAYSURF, BLUE)
+
 
 # Set up Groups
 people = pygame.sprite.Group()
-people.add(P1)
-all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
+#people.add(P1)
 
-# More stuff
-INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
+all_sprites = pygame.sprite.Group()
+#all_sprites.add(P1)
+
+for i in range(10):
+    P = person.Person(DISPLAYSURF, BLUE)
+    people.add(P)
+    all_sprites.add(P)
+
 
 # Main loop
 while True:
     # parse all events
     for event in pygame.event.get():
-        if event.type == INC_SPEED:
-            SPEED += 2
-
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
--- person.py
+++ person.py
@@ -5,11 +5,11 @@
 #############
 
 #Imports
-import pygame
+import pygame, random
 
 class Person(pygame.sprite.Sprite):
     #Constructor
-    def __init__(self, color, width = 10, height = 10):
+    def __init__(self, screen, color = [125,125,125], width = 10, height = 10):
         #parent class constructor
         pygame.sprite.Sprite.__init__(self)
 
@@ -17,7 +17,10 @@ class Person(pygame.sprite.Sprite):
         self.image = pygame.Surface([width,height])
         self.image.fill(color)
 
-        self.rect = self.image.get_rect()
+        # store a  reference to the screen, for positioning and movement
+        self.DISPLAYSURF = screen
+
+        self.rect = self.image.get_rect(center = (random.randint(0,self.DISPLAYSURF.get_width()),random.randint(0,self.DISPLAYSURF.get_height())))
 
     def update():
         pass
