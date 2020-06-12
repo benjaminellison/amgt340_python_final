fb5e2dbe971e233e207fd7bf51aa9f7e65835ed0
benjaminellison <scorpio.breeze@gmail.com>
2020-06-12 07:55:23 -0700

    added more comments

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ all_sprites = pygame.sprite.Group()
 healthy_sprites = pygame.sprite.Group()
 sick_sprites = pygame.sprite.Group()
 
-for i in range(30):
+# add 50 sprites to the screen
+for i in range(50):
+    # there's a 93% chance a sprite is healthy
     healthy = random.random() > .07
     P = person.Person(DISPLAYSURF, healthy)
     all_sprites.add(P)
@@ -49,18 +51,29 @@ while True:
             pygame.quit()
             quit(0)
 
+    # fill the background with solid white
     DISPLAYSURF.fill(WHITE)
 
+    # find and iterate over the collisions between sick sprites and healthy ones
+    # groupcollide returns a dictionary where  all the sick ones are keys and the values are a list of healthy collisions
     overlap = pygame.sprite.groupcollide(sick_sprites, healthy_sprites, False, False)
     for sicko, exposedPeeps in overlap.items():
+        # every healthy sprite that overlaps a sick one.
         for exposed in exposedPeeps:
-            sick_sprites.add(exposed)
+            # toggle the newly infected sprite attribute and
+            # shuffle it between groups
             exposed.healthy = False
+            sick_sprites.add(exposed)
+            healthy_sprites.remove(exposed)
+            
 
+    # call the update routine for all the sprites
     all_sprites.update()
 
+    # paint the changes to the screen
     pygame.display.update()
 
+    # waits a set amount of time
     FramePerSec.tick(FPS)
 
 
--- person.py
+++ person.py
@@ -41,6 +41,8 @@ class Person(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center = (random.randint(0,self.DISPLAYSURF.get_width()),random.randint(0,self.DISPLAYSURF.get_height())))
 
     def update(self):
+        # boundary checking - if the sprite is moving off the screen, reverse the direction
+        # this causes almost a "bounce"
         if self.move_x > 0 and (self.rect.right + self.move_x) > self.DISPLAYSURF.get_width():
             self.move_x *= -1
 
@@ -53,20 +55,32 @@ class Person(pygame.sprite.Sprite):
         if self.move_y < 0 and (self.rect.top - self.move_y) < 0:
             self.move_y *= -1
 
+        # move the sprite by it's "velocity"
         self.rect.move_ip(self.move_x, self.move_y)
+
+        # we try to keep a sprite moving in one direction for a period of time
+        # if we didn't keep track of this, the sprites would either move in one direction
+        # until they hit a wall, like a ball/bullet, or jitter/vibrate endlessly
+        # keeping track of the time since they last changed vectors allows us to 
+        # have sprites that "wander" around
         self.TSLT += 1
         if self.TSLT > random.randint(10,100):
             self.TSLT = 0
             self.move_x = random.randint(-2,2)
             self.move_y = random.randint(-2,2)
         
+        # if for some crazy reason the boundary checks/bouncing above doesn't work,
+        # this will reset the position of the sprite. Probably not strictly necessary now 
+        # (it was early code), so it could be removed for efficiency
         if not self.DISPLAYSURF.get_rect().colliderect(self.rect):
             self.rect = self.image.get_rect(center = (random.randint(0,self.DISPLAYSURF.get_width()),random.randint(0,self.DISPLAYSURF.get_height()))) 
 
+        # Healthy is Green, Sick is Red.
         if self.healthy:
             self.image.fill([0, 255, 0])
         else:
             self.image.fill([255, 0, 0])
 
+        # Paint ourself to the surface that will get displayed
         self.DISPLAYSURF.blit(self.image, self.rect)
 
